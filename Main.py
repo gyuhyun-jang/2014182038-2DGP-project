c6f283a5f4211c38e4fa55e891512001098bff8f
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2019-10-18 18:47:09 +0900

    Update Main.py
    총알발사구현

--- Main.py
+++ Main.py
@@ -38,32 +38,49 @@ class Enemy:
         self.image.draw(self.x, self.y)
 
 
+class Bullet:
+    def __init__(self):
+        self.x, self.y = 0, 0
+        self.image = load_image('bullet.png')
+        self.speed = 10
+        self.count = 0
+
+    def update(self):
+        if self.x != 0:
+            self.y += self.speed
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
 
 def initGame():
-    global running_game, background, clock, player, enemys
+    global running_game, background, clock, player, enemys, bullets, count
     running_game = True
+    count = 0
     open_canvas(padWidth, padWidth)
     #background = load_image('background001.png')
     player = Player()
     enemys = [Enemy() for i in range(10)]
-    bullet = load_image('bullet.png')
+    bullets = [Bullet() for i in range(100)]
 
 def runGame():
-    global running_game, background, clock, player, enemys
+    global running_game, background, clock, player, bullets, enemys
     #background.draw(padWidth//2, padHeight //2)
     player.update()
     for enemy in enemys:
         enemy.update()
+    for bullet in bullets:
+        bullet.update()
     clear_canvas()
     player.draw()
     for enemy in enemys:
         enemy.draw()
-
+    for bullet in bullets:
+        bullet.draw()
     update_canvas()
 
 
 def handle_events():
-    global running_game, player
+    global running_game, player, bullets, count
 
     events = get_events()
     for event in events:
@@ -76,6 +93,10 @@ def handle_events():
                 player.dir += 1
             elif event.key == SDLK_LEFT:
                 player.dir -= 1
+            elif event.key == SDLK_SPACE:
+                bullets[count].x = player.x
+                bullets[count].y = player.y + 10
+                count += 1
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 player.dir -= 1
@@ -85,7 +106,6 @@ def handle_events():
 
 
 initGame()
-
 while running_game:
     runGame()
     handle_events()

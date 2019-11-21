b0ce47c350860c622965fb7067d7e95b63d49174
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2019-11-22 00:56:51 +0900

    player update

--- player.py
+++ player.py
@@ -1,24 +1,142 @@
 from pico2d import*
+import game_framework
+import game_world
+import main_state
+from bullet import Bullet
 
-padWidth,padHeight = 400,600
+
+# PLAYER 42 X 42
+
+
+padWidth, padHeight = 400,600
+
+PIXEL_PER_METER = (10.0 / 0.5)  # 10 pixel 50 CM
+RUN_SPEED_KMPH = 30.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 4
+
+RIGHT_DOWN, RIGHT_UP, LEFT_DOWN, LEFT_UP, SPACE = range(5)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYDOWN, SDLK_SPACE): SPACE
+}
+
+class IdleState:
+
+    @staticmethod
+    def enter(player, event):
+        if event == RIGHT_DOWN:
+            player.velocity += RUN_SPEED_PPS
+        elif event == LEFT_DOWN:
+            player.velocity -= RUN_SPEED_PPS
+        elif event == RIGHT_UP:
+            player.velocity -= RUN_SPEED_PPS
+        elif event == LEFT_UP:
+            player.velocity += RUN_SPEED_PPS
+        player.dir = clamp(-1, player.velocity, 1)
+
+
+
+    @staticmethod
+    def exit(player, event):
+        if event == SPACE:
+            player.shoot_bullet()
+        pass
+
+    @staticmethod
+    def do(player):
+        player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+
+    @staticmethod
+    def draw(player):
+        player.image.clip_draw(int(player.frame) * 42, 84, 42, 42, player.x, player.y)
+
+
+class RunState:
+
+    @staticmethod
+    def enter(player, event):
+        if event == RIGHT_DOWN:
+            player.velocity += RUN_SPEED_PPS
+        elif event == LEFT_DOWN:
+            player.velocity -= RUN_SPEED_PPS
+        elif event == RIGHT_UP:
+            player.velocity -= RUN_SPEED_PPS
+        elif event == LEFT_UP:
+            player.velocity += RUN_SPEED_PPS
+        player.dir = clamp(-1, player.velocity, 1)
+
+
+    @staticmethod
+    def exit(player, event):
+        if event == SPACE:
+            player.shoot_bullet()
+        pass
+
+    @staticmethod
+    def do(player):
+        player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        player.x += player.velocity * game_framework.frame_time
+        player.x = clamp(0, player.x, 400)
+
+
+    @staticmethod
+    def draw(player):
+        if player.dir == 1:
+            player.image.clip_draw(int(player.frame) * 42, 42, 42, 42, player.x, player.y)
+        elif player.dir == -1:
+            player.image.clip_draw(int(player.frame) * 42, 0, 42, 42, player.x, player.y)
+
+
+next_state_table = {
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, SPACE: IdleState},
+    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, RIGHT_DOWN: IdleState, LEFT_DOWN: IdleState, SPACE: RunState}
+}
+
 
 class Player:
     def __init__(self):
         self.x, self.y = padWidth // 2, 20
+        self.image = load_image('./resource/animation01.png')
         self.frame = 0
         self.dir = 0
-        self.image = load_image('animation01.png')
+        self.velocity = 0
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self, None)
+        self.bullet = None
+
+    def get_bb(self):
+        return self.x - 21, self.y - 21, self.x + 21, self.y + 21
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
 
     def update(self):
-        self.frame = (self.frame + 1) % 4
-        self.x += self.dir * 5
-        self.x = clamp(20,self.x,380)
-        if self.dir < 0:
-            self.state = 0
-        elif self.dir > 0:
-            self.state = 1
-        elif self.dir == 0:
-            self.state = 2
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
 
     def draw(self):
-        self.image.clip_draw(self.frame*42, 42 * self.state, 42, 42, self.x, self.y)
+        self.cur_state.draw(self)
+
+    def handle_event(self, event):
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
+
+    def shoot_bullet(self):
+        game_world.add_object(Bullet(), 1)
+        pass

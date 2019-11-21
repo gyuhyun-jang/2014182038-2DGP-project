988c4a006a01e798388bb800fe1eab997f1f3952
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2019-11-22 00:54:33 +0900

    bullet time 적용

--- bullet.py
+++ bullet.py
@@ -1,16 +1,42 @@
 from pico2d import *
-from resource import *
-import resource
+import game_world
+import game_framework
+import enemy
+import player
+import main_state
+
+
+PIXEL_PER_METER = (10.0 / 0.5)  # 10 pixel 50 cm
+RUN_SPEED_KMPH = 0.1  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
 
 class Bullet:
-    def __init__(self):
-        self.x, self.y = 0, 0
-        self.image = load_image('bullet.png')
-        self.speed = 12
+    image = None
 
-    def update(self):
-        if self.x != 0:
-            self.y += self.speed
+    def __init__(self):
+        if Bullet.image is None:
+            Bullet.image = load_image('./resource/bullet.png')
+        self.x, self.y, self.speed = main_state.player.x, main_state.player.y, RUN_SPEED_PPS
+        self.damage = 1
+
+    def get_bb(self):
+        return self.x - 4, self.y - 9, self.x + 4, self.y + 9
 
     def draw(self):
         self.image.draw(self.x, self.y)
+
+    def update(self):
+        self.y += self.speed
+
+        if self.y > 600:
+            game_world.remove_object(self)
+
+    def hit_enemy(self):
+        game_world.remove_object(self)
+
+
+
+

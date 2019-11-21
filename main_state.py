a79271da69e4617372de90e6693ef34c918c3a7a
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2019-11-22 00:55:35 +0900

    game_world 적용

--- main_state.py
+++ main_state.py
@@ -4,7 +4,7 @@ import os
 import time
 
 from pico2d import *
-
+import game_world
 import game_framework
 import title_state
 import pause_state
@@ -12,6 +12,7 @@ import pause_state
 from player import Player
 from enemy import Enemy
 from bullet import Bullet
+from background import Background
 
 padWidth, padHeight = 400, 600
 name = "MainState"
@@ -19,22 +20,50 @@ name = "MainState"
 player = None
 enemy = None
 bullet = None
+enemys = []
+kill_score = 0
+
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+
+def get_player():
+    return player
+
+
+def get_bullet():
+    return bullet
 
 
 def enter():
-    global running_game, player, enemys, bullets, count, background
+    global running_game
     running_game = True
-    open_canvas(padWidth, padHeight)
-    background = load_image('background_01.png')
+
+    global player
     player = Player()
-    enemys = [Enemy()]
-    bullets = [Bullet()]
-    count = 0
-    pass
+    game_world.add_object(player, 1)
+
+
+    global enemys, amount_of_enemys
+    amount_of_enemys = 20
+    enemys = [Enemy() for i in range(amount_of_enemys)]
+    game_world.add_objects(enemys, 1)
+
+    background = Background()
+    game_world.add_object(background, 0)
 
 
 def exit():
-    close_canvas()
+    game_world.clear()
     pass
 
 
@@ -47,73 +76,30 @@ def resume():
 
 
 def handle_events():
-    global running_game, player, bullets, count, enemys
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:
+        if event.type == SDL_Quit:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
-            game_framework.push_state(pause_state)
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE:
-                running_game = False
-            elif event.key == SDLK_RIGHT:
-                player.dir += 1
-            elif event.key == SDLK_LEFT:
-                player.dir -= 1
-            elif event.key == SDLK_SPACE:
-                bullets[count].x, bullets[count].y = player.x, player.y + 10
-                count += 1
-                if count == 20:
-                    count = 0
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                player.dir -= 1
-            elif event.key == SDLK_LEFT:
-                player.dir += 1
+        else:
+            player.handle_event(event)
 
 
 def update():
-    global hp, max_enemy, kill_score, max_bullet_amount
-    hp = 5
-    max_enemy, max_bullet_amount = 20, 20
-    kill_score = 0
-    player.update()
+    for game_object in game_world.all_objects():
+        game_object.update()
     for enemy in enemys:
-        enemy.update()
-    for bullet in bullets:
-        bullet.update()
-    # 충돌체크
-    for enemy in enemys:
-        if enemy.y < 30:
-            if hp == 0:
-                game_framework.change_state(title_state)
-            hp -= 1
-        for bullet in bullets:
-            if enemy.x + 16 >= bullet.x >= enemy.x - 16 and enemy.y + 10 >= bullet.y >= enemy.y - 10:
-                enemys.remove(enemy)
-                bullets.remove(bullet)
-                kill_score += 1
-                break
-    while len(enemys) < max_enemy:
-        enemys.append(Enemy())
-    while len(bullets) < max_bullet_amount:
-        bullets.append(Bullet())
-    if kill_score % 10 == 0:
-        max_enemy += 1
+        if collide(bullet, enemy):
+            bullet.hit_enemy()
+            enemy.get_damaged(bullet.damage)
 
-    delay(0.02)
-    pass
+
 
 
 def draw():
     clear_canvas()
-    background.draw(padWidth // 2, padHeight // 2)
-    player.draw()
-    for enemy in enemys:
-        enemy.draw()
-    for bullet in bullets:
-        bullet.draw()
+    for game_object in game_world.all_objects():
+        game_object.draw()
     update_canvas()
+

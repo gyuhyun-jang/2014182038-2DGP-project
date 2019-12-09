177756200e64878f9a4eaa48bafd450c6581c113
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2019-12-10 05:06:07 +0900

    enemy 객체 최종 완료

--- enemy.py
+++ enemy.py
@@ -2,47 +2,61 @@ from pico2d import *
 import random
 import game_framework
 import game_world
-
+import main_state
 PIXEL_PER_METER = (10.0 / 0.5)  # 10 pixel 50 cm
-RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_KMPH = 10.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-SPEED_INCREASE = 1.0
 
-enemy_type = ['./resource/enemy/rock02.png',
-              './resource/enemy/rock14.png',
-              './resource/enemy/rock18.png']
+enemy_type = ['./resource/enemy/rock0_0.png', './resource/enemy/rock0_1.png', './resource/enemy/rock0_2.png',
+              './resource/enemy/rock1_0.png', './resource/enemy/rock1_1.png', './resource/enemy/rock1_2.png',
+              './resource/enemy/rock2_0.png', './resource/enemy/rock2_1.png', './resource/enemy/rock2_2.png']
+enemy_size = [60, 50, 40]
 
 
-class Enemy():
+class Enemy:
     image = None
+    explosion_image = None
 
     def __init__(self):
-        if Enemy.image == None:
-            self.image = load_image(enemy_type[0])
-        self.x, self.y, self.fall_speed = random.randint(40, 370), 640, RUN_SPEED_PPS * SPEED_INCREASE
-        self.hp = 1
+        if Enemy.image is None:
+            self.image = load_image(enemy_type[random.randint(0, 2)])
+            self.explosion_image = load_image('./resource/explosion.png')
+            self.size = enemy_size[0]
+        self.x, self.y = random.randint(40, 370), 640
+        self.fall_speed = RUN_SPEED_PPS
+        self.hp = main_state.enemy_life
+        self.explosion_sound = load_wav('./resource/sound/explosion.wav')
+        self.explosion_sound.set_volume(2)
 
 
     def get_bb(self):
-        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+        return self.x - self.size//2, self.y - self.size//2, self.x + self.size, self.y + self.size
 
     def update(self):
         self.y -= self.fall_speed * game_framework.frame_time
 
         if self.y < 0:
-            game_world.remove_object(self)
+            self.explosion_sound.play()
+            main_state.player_life -= 1
+            self.hp = main_state.enemy_life
+            main_state.kill_score += 1
+            self.x, self.y = random.randint(40, 370), 640
 
         if self.hp <= 0:
-            game_world.remove_object(self)
+            self.explosion_sound.play()
+            self.hp = main_state.enemy_life
+            main_state.kill_score += 1
+            self.x, self.y = random.randint(40, 370), 640
+            main_state.money += 1
 
     def draw(self):
-        self.image.draw(self.x, self.y)
-
-    def get_damaged(self, x):
-        self.hp -= x
+        if self.hp <= 0:
+            self.explosion_image.draw(self.x, self.y)
+        else:
+            self.image.draw(self.x, self.y)
 
     def exit(self):
         pass

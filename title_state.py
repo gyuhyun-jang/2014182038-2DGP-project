fffc68cd83cc962c1f899556c96a399c559b136d
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2019-12-10 05:04:11 +0900

    title state 수정

--- title_state.py
+++ title_state.py
@@ -1,15 +1,16 @@
 import game_framework
 import main_state
+import help_state
 from pico2d import *
 
 
-name = "TitleState"
+name = "StartState"
 image = None
+logo_time = 0.0
 
 
 def enter():
     global image
-    open_canvas(800,600)
     image = load_image('./resource/title.png')
 
 
@@ -18,33 +19,32 @@ def exit():
     del(image)
 
 
-def handle_events():
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            game_framework.quit()
-        else:
-            if(event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
-                game_framework.quit()
-            elif(event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-                game_framework.change_state(main_state)
+def update():
+    global logo_time, image
+
+    if (logo_time > 1.0):
+        logo_time = 0
+        game_framework.change_state(help_state)
+    delay(0.01)
+    logo_time += 0.01
 
 
 def draw():
+    global image
     clear_canvas()
-    image.draw(800 // 2, 600 // 2)
+    image.draw(200, 300)
     update_canvas()
 
-def update():
-    pass
 
-def pause():
-    pass
-
-def resume():
+def handle_events():
+    events = get_events()
     pass
 
 
+def pause(): pass
+
+
+def resume(): pass
 
 
 

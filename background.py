feb3ec3374d7757f3c43c4c1cecbe0eb23dc4479
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2019-12-10 05:02:55 +0900

    bgm 추가

--- background.py
+++ background.py
@@ -1,11 +1,14 @@
 from pico2d import *
+import main_state
 
 padWidth, padHeight = 400, 600
-
 
 class Background:
     def __init__(self):
         self.image = load_image('./resource/background/background_01.png')
+        self.bgm = load_music('./resource/sound/bgm4.mp3')
+        self.bgm.set_volume(20)
+        self.bgm.repeat_play()
 
     def update(self):
         pass
